70b6bc3fe857d0ddb2c55ce308493bfc8d534ba7
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-01-02 23:47:23 +0900

    image delete

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -6,7 +6,7 @@ if(session('status') === 'alert'){ $bgColor = 'bg-red-500 border-0 py-2 px-8 foc
 @endphp
 
 @if (session('message'))
-    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 my-4 text-white">
         {{ session('message') }}
     </div>
 @endif
