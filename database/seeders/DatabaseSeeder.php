f4b69d7ddcdcaaef0bda8ab890a77be5f2e52401
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-01-03 19:18:53 +0900

    route itemcontrollre

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             // ProductSeeder::class,
             // StockSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
