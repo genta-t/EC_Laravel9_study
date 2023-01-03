6ad7f75cf3818f3092eb9994ebb9a09c0789cf39
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-01-03 18:27:01 +0900

    route修正

--- routes/owner.php
+++ routes/owner.php
@@ -28,10 +28,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('owner.welcome');
-});
-
 Route::prefix('shops')
     ->middleware('auth:owners')
     ->group(function () {
@@ -59,11 +55,6 @@ Route::middleware('auth:owners')->group(function () {
 
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
