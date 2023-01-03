deaaaafdc1479ea73b3e8742037021699adea228
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-01-03 21:13:34 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,9 +11,26 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = ($request->route()->parameter('product'));
+            if (!is_null($id)) {
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId;
+                if ($productId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-
         $stocks = DB::table('t_stocks')
             ->select(
                 'product_id',
@@ -47,5 +64,12 @@ class ItemController extends Controller
             ->get();
 
         return view('user.index', compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
